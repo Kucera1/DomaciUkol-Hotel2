4a8022b70393e753569bdd32e824e0b0e77adf32
Kucera1 <156932274+Kucera1@users.noreply.github.com>
2024-02-22 15:22:13 +0100

    vytvoření třídy BookingManager přidání metod addBooking, getBookings, clearBookings, getNumberOfWorkingBookings, getAverageGuests

--- src/com/engeto/ja/du1/com/engeto/ja/du1/BookingManager.java
+++ src/com/engeto/ja/du1/com/engeto/ja/du1/BookingManager.java
@@ -1,4 +1,40 @@
 package com.engeto.ja.du1.com.engeto.ja.du1;
 
+import com.engeto.ja.du1.Booking;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BookingManager {
+    private List<Booking> bookings = new ArrayList<>();
+    public void addBooking(Booking booking) {
+        bookings.add(booking);
+    }
+    public void getBookings() {
+        for (Booking booking : bookings) {
+            System.out.println(booking);
+        }
+    }
+    public void clearBookings() {
+        bookings.clear();
+    }
+
+    public void getNumberOfWorkingBookings() {
+        int count = 0;
+        for (Booking booking : bookings) {
+            if (booking.getFrom().isBefore(booking.getTo())) {
+                count++;
+            }
+        }
+        System.out.println("Number of working bookings: " + count);
+    }
+    public void getAverageGuests () {
+        int count = 0;
+        for (Booking booking : bookings) {
+            count += booking.getGuests().size();
+        }
+        System.out.println("Average number of guests: " + count / bookings.size());
+    }
+
+
 }
